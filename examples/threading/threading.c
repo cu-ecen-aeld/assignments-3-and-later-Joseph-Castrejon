5e4f3864ebc64839413fbd9565c6c7822ff14b34
Joseph Castrejon <JCastrejon95@protonmail.com>
2024-04-08 07:48:10 -0500

    Add code for assignment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -4,30 +4,78 @@
 #include <stdio.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+//#define DEBUG_LOG(msg,...)
+#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
 void* threadfunc(void* thread_param)
 {
+	DEBUG_LOG("Entering a new thread.");
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+	// TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
+	// hint: use a cast like the one below to obtain thread arguments from your parameter
+
+	struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+	usleep(thread_func_args->obtain_ms*1000);
+
+	if(pthread_mutex_lock(thread_func_args->mutex) != 0){
+		ERROR_LOG("Could not lock mutex!");
+		return thread_param;
+	};
+
+	DEBUG_LOG("Thread obtained mutex, waiting for %d ms",thread_func_args->release_ms);
+
+	usleep(thread_func_args->release_ms*1000);
+
+	if(pthread_mutex_unlock(thread_func_args->mutex) != 0){
+		ERROR_LOG("Could not unlock mutex!");
+		return thread_param;
+	};
+
+	DEBUG_LOG("Thread released mutex");
+
+	thread_func_args->thread_complete_success = true;
+
+	return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
+	/**
+	* TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
+	* using threadfunc() as entry point.
+	*
+	* return true if successful.
+	*
+	* See implementation details in threading.h file comment block
+	*/
+
+	struct thread_data* data = (struct thread_data*) malloc(sizeof(struct thread_data));
+
+	/* Unable to allocate memory */
+	if(data == NULL){
+	    ERROR_LOG("Out of memory!");
+	    return false;
+	}
+
+	/* Set thread_data */ 
+	data->obtain_ms = wait_to_obtain_ms;
+	data->release_ms = wait_to_release_ms;
+	data->mutex = mutex;
+
+	DEBUG_LOG("Creating new thread.");
+
+	int rc = pthread_create(thread, NULL, threadfunc, (void *) data);
+		
+	if( rc != 0){
+	    ERROR_LOG("Couldn't create thread!");
+	    free(data);
+	    return false;
+	}	
+
+	DEBUG_LOG("Thread created succesfully.");
+	return true;
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -8,18 +8,21 @@
  * the joiner thread.
  */
 struct thread_data{
-    /*
-     * TODO: add other values your thread will need to manage
-     * into this structure, use this structure to communicate
-     * between the start_thread_obtaining_mutex function and
-     * your thread implementation.
-     */
-
-    /**
-     * Set to true if the thread completed with success, false
-     * if an error occurred.
-     */
-    bool thread_complete_success;
+	/*
+	* TODO: add other values your thread will need to manage
+	* into this structure, use this structure to communicate
+	* between the start_thread_obtaining_mutex function and
+	* your thread implementation.
+	*/
+	int obtain_ms;
+	int release_ms;	
+	pthread_mutex_t* mutex;
+	pthread_t* thread;		
+	/**
+	* Set to true if the thread completed with success, false
+	* if an error occurred.
+	*/
+	bool thread_complete_success;
 };
 
 
